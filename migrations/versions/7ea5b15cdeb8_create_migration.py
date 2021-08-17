2a8caa1ebab8db7ea855f33af150310479ac36e7
Özgün Gökşenli <ogoksenli@kocfinans.com.tr>
2021-08-17 17:09:34 +0300

    Change migration

--- migrations/versions/7ea5b15cdeb8_create_migration.py
+++ migrations/versions/7ea5b15cdeb8_create_migration.py
@@ -19,6 +19,7 @@ branch_labels = None
 depends_on = None
 
 
+
 def upgrade():
     op.create_table(
         'artists',
@@ -66,9 +67,66 @@ def upgrade():
         sa.PrimaryKeyConstraint('id'),
         )
 
+    op.create_table(
+        'PastShow',
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column('name', sa.String(), nullable=True),
+        sa.Column('venue_id', sa.Integer(), nullable=False),
+        sa.Column('artist_id', sa.Integer(), nullable=False),
+        sa.Column('start_time', sa.DateTime(), nullable=True),
+        sa.ForeignKeyConstraint(['artist_id'], ['Artist.id']),
+        sa.ForeignKeyConstraint(['venue_id'], ['Venue.id']),
+        sa.PrimaryKeyConstraint('id'),
+        )
+    op.create_table(
+        'UpcomingShow',
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column('name', sa.String(), nullable=True),
+        sa.Column('venue_id', sa.Integer(), nullable=False),
+        sa.Column('artist_id', sa.Integer(), nullable=False),
+        sa.Column('start_time', sa.DateTime(), nullable=True),
+        sa.ForeignKeyConstraint(['artist_id'], ['Artist.id']),
+        sa.ForeignKeyConstraint(['venue_id'], ['Venue.id']),
+        sa.PrimaryKeyConstraint('id'),
+        )
+    op.drop_table('Show')
+    op.create_table('Area', sa.Column('id', sa.Integer(),
+                    nullable=False), sa.Column('city',
+                    sa.String(length=120), nullable=True),
+                    sa.Column('state', sa.String(length=120),
+                    nullable=True), sa.PrimaryKeyConstraint('id'))
+    op.add_column('Venue', sa.Column('area_id', sa.Integer(),
+                  nullable=False))
+    op.create_foreign_key(None, 'Venue', 'Area', ['area_id'], ['id'])
+
 
 def downgrade():
 
     op.drop_table('shows')
     op.drop_table('venues')
     op.drop_table('artists')
+    op.drop_constraint(None, 'Venue', type_='foreignkey')
+    op.drop_column('Venue', 'area_id')
+    op.drop_table('Area')
+    op.create_table(
+        'Show',
+        sa.Column('id', sa.INTEGER(),
+                  server_default=sa.text('nextval(\'"Show_id_seq"\'::regclass)'
+                  ), autoincrement=True, nullable=False),
+        sa.Column('name', sa.VARCHAR(), autoincrement=False,
+                  nullable=True),
+        sa.Column('venue_id', sa.INTEGER(), autoincrement=False,
+                  nullable=False),
+        sa.Column('artist_id', sa.INTEGER(), autoincrement=False,
+                  nullable=False),
+        sa.Column('start_time', postgresql.TIMESTAMP(),
+                  autoincrement=False, nullable=True),
+        sa.ForeignKeyConstraint(['artist_id'], ['Artist.id'],
+                                name='Show_artist_id_fkey'),
+        sa.ForeignKeyConstraint(['venue_id'], ['Venue.id'],
+                                name='Show_venue_id_fkey'),
+        sa.PrimaryKeyConstraint('id', name='Show_pkey'),
+        )
+    op.drop_table('UpcomingShow')
+    op.drop_table('PastShow')
+
